275c9e243e00c2771e2d079b0c8c45c62ba70e9b
goutham2204 <gouthamkumar45@gmail.com>
2018-07-17 12:11:23 -0700

    add product done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public pd_code: string;
+  public pd_name: string;
+  public pd_price: number;
+  public pd_gst: number;
+  constructor() {
+
+  }
+
+  addProduct() {
+    const product: any = {
+      code: this.pd_code,
+      name: this.pd_name,
+      price: this.pd_price,
+      gst: this.pd_gst
+    };
+    console.log('###', product);
+    let products: any[] = JSON.parse(window.localStorage.getItem('products'));
+    console.log('@@@@@@@@@@@', products);
+    if (!products) {
+      products = [];
+    }
+    products.push(product);
+    window.localStorage.setItem('products', JSON.stringify(products));
+    this.pd_code = null;
+    this.pd_name = null;
+    this.pd_price = null;
+    this.pd_gst = null;
+  }
 }
